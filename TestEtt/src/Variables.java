eee83768374e3239cf928d5f4a9467214f25b801
MariaBahia <hejmariabahia@gmail.com>
2016-11-10 14:22:31 +0100

    lpål

---
+++ TestEtt/src/Variables.java
@@ -0,0 +1,18 @@
+
+public class Variables {
+	
+	private static int c;  // private varible and must be added a parameter
+
+
+	public static void main(String[] args) {
+		c=45;								// adding a public value to c
+		String s="Linda";					// s has been given a value and can be printed
+		String k="maria";					// k has been given a value and can be printed
+		System.out.println(s);
+		System.out.println(c);
+		System.out.println(k);
+
+
+	}
+
+}
